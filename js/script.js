6e7ad3f535966990ede0856394bd9fd5d3cd73c5
Michel França <fr.mich17@gmail.com>
2021-02-15 16:09:55 -0300

    Learning TailwindCSS

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const searchBar = document.querySelector('.main__search-bar')
+const figure = document.querySelector('.main__last-releases___thumbs____content')
+const titles = document.querySelectorAll('.main__last-releases___thumbs____content_____title-text')
+const thumbs = document.querySelector('.main__last-releases___thumbs img')
+
+const changeColorToggle = () => {
+    searchBar.classList.toggle('changecolor-border')
+}
+searchBar.addEventListener('click', changeColorToggle)
+searchBar.addEventListener('focusout', changeColorToggle)
+
+
+const limitedCharacters = (element, index) => {
+    if (element.textContent.length > 36) {
+        titles[index].innerHTML = element.textContent.slice(0, 33) + '...'
+    }
+}
+titles.forEach(limitedCharacters)
